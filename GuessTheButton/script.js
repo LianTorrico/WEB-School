a5ce0964353767f72932c10609d192abec90cfc8
Lian Torrico <LIAN.TORRICO@local.ittcesena.it>
2025-05-13 12:52:55 +0200

    Test WebSocket2

--- GuessTheButton/script.js
+++ GuessTheButton/script.js
@@ -1,15 +1,33 @@
 var webSocketServer = new (require('ws')).Server({port: (process.env.PORT || 5000)}),
     webSockets = {} // userID: webSocket
-    var userID = parseInt(webSocket.upgrageReq.url.substr(1), 10)
-    webSockets[userID] = webSocket
-window.ws = sock
 
-var clients = [];
-var Masterr=null;
-if (Response.method==="connect"){
-    Masterr = Response.Masterr
-}
-function Master() {
-    document.getElementById("btnMast").remove();
+// CONNECT /:userID
+// wscat -c ws://localhost:5000/1
+webSocketServer.on('connection', function (webSocket) {
+  var userID = parseInt(webSocket.upgradeReq.url.substr(1), 10)
+  webSockets[userID] = webSocket
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(webSockets))
 
-}
+  // Forward Message
+  //
+  // Receive               Example
+  // [toUserID, text]      [2, "Hello, World!"]
+  //
+  // Send                  Example
+  // [fromUserID, text]    [1, "Hello, World!"]
+  webSocket.on('message', function(message) {
+    console.log('received from ' + userID + ': ' + message)
+    var messageArray = JSON.parse(message)
+    var toUserWebSocket = webSockets[messageArray[0]]
+    if (toUserWebSocket) {
+      console.log('sent to ' + messageArray[0] + ': ' + JSON.stringify(messageArray))
+      messageArray[0] = userID
+      toUserWebSocket.send(JSON.stringify(messageArray))
+    }
+  })
+
+  webSocket.on('close', function () {
+    delete webSockets[userID]
+    console.log('deleted: ' + userID)
+  })
+})
